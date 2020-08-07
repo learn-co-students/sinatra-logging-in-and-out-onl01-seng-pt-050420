a5363a1804f897e4cedb1236d27b98d7e4966b95
Kevin Boston <kbos22@gmail.com>
2020-08-07 06:42:51 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+    def self.current_user(the_session)
+        @user = User.find_by_id(the_session[:user_id])
+    end
+
+    def self.is_logged_in?(the_session)
+        !!the_session[:user_id]
+    end
+
+
+
+
 end
