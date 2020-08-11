e2bb4c8e3d90b622444b4051c68261dba7127022
tpham13 <tpham0123@gmail.com>
2020-08-11 12:02:58 -0700

    Done.

--- db/migrate/create_users.rb
+++ db/migrate/create_users.rb
@@ -1,11 +1,11 @@
-class CreateUsers < ActiveRecord::Migration[4.2]
+class CreateUsers < ActiveRecord::Migration[5.1]
 
     def change 
         create_table :users do |t|
 
         t.string :username
         t.string :password
-        t.float :balance
+        t.decimal :balance
         end 
     end 
 end 
