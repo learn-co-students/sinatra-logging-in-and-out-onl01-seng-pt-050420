b64267a70913d8ecf6fe21ef41ec2350bf9c095c
dblant <dustinblntn@gmail.com>
2020-08-14 17:50:47 -0500

    Done.

---
+++ db/migrate/20200814010300_creates_users_table.rb
@@ -0,0 +1,11 @@
+class CreatesUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.text :username
+      t.text :password
+      t.float :balance
+    
+    end
+
+  end
+end
